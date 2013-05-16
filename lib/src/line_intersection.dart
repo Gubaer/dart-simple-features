9dd9abe4849758fc1d18ee950962f0c5bd91851a
Gubaer <karl.guggisberg@guggis.ch>
2013-05-16 17:23:11 +0200

    implemented first version of Bentley-Ottman-Algorithm for line intersection

---
+++ lib/src/line_intersection.dart
@@ -0,0 +1,496 @@
+part of line_intersection;
+
+_require(cond, [msg]) {
+    if (!cond) throw new ArgumentError(msg == null ? "" : msg);
+}
+
+/**
+ * Computes the counterclockwise orientation of point [r] with respect
+ * to the line given by the start and end points [p] and [q].
+ *
+ * Returns -1 if, if [r] is left to [p]-[q], 0 if is colinear with [p]-[q]
+ * and 1 if it is to the right of [p]-[q].
+ */
+int _orientation(DirectPosition2D p, DirectPosition2D q, DirectPosition2D r) {
+  var v = (p.x - r.x) * (q.y - r.y) - (q.x - r.x) * (p.y - r.y);
+  if (v < 0) return -1;
+  if (v == 0) return 0;
+  return 1;
+}
+
+/// determinante
+num det(p, q) => p.x * q.y - p.y * q.x;
+
+/**
+ * Represents closed line segment given by two 2D points.
+ *
+ * [start] is always the end point with the larger y-coordinate, or,
+ * if y is equal, the lower x-coordinate. This is the order used
+ * on events in the line intersection algorithm.
+ *
+ * LineSegments are never collapsed, they always have exactly two distinct
+ * end points.
+ */
+class LineSegment {
+  DirectPosition2D _start;
+  DirectPosition2D _end;
+
+  /**
+   * Creates a line segment with start position [start] and end position
+   * [end]. [start] and [end] don't have to be ordered, but the position
+   * with the higher y-coordinate, or, if y is equal, the lower x-coordinate
+   * becomes the [start] event. [start] and [end] must not be equal.
+   *
+   */
+  LineSegment(DirectPosition2D start, DirectPosition2D end) {
+    _require(start != end, "line segments must not be collapsed: $start, $end");
+    int c = _comparePositionsInEventOrder(start, end);
+    if (c <= 0) {
+      _start = start;
+      _end = end;
+    } else {
+      _start = end;
+      _end = start;
+    }
+  }
+
+  /**
+   * Creates a line segment from a [start] and [end] event, where
+   * [start] and [end] are list of [num]s with two elements.
+   *
+   * # Example
+   *     var s = new LineSegment.from([0,0], [2,2]);
+   */
+  factory LineSegment.from(start, end) {
+    _require(start is List);
+    _require(end is List);
+    _require(start.length == 2);
+    _require(end.length == 2);
+    return new LineSegment(
+        new DirectPosition2D(start[0], start[1]),
+        new DirectPosition2D(end[0], end[1])
+    );
+  }
+
+  /// returns true, if [p] is either the start or the end point
+  /// of this segment
+  bool isEndPoint(DirectPosition2D p) => p == _start || p == _end;
+
+  bool _fastExcludeIntersect(LineSegment other) {
+    var minx = _start.x;
+    var miny = _start.y < _end.y ? _start.y : _end.y;
+    var maxx = _end.x;
+    var maxy = _start.y >= _end.y ? _start.y : _end.y;
+    if (other._start.x < minx && other._end.x < minx) return true;
+    if (other._start.x > maxx && other._end.x > maxx) return true;
+    if (other._start.y < miny && other._end.y < miny) return true;
+    if (other._start.y > maxy && other._end.y > maxy) return true;
+    return false;
+  }
+
+  /**
+   * This [LineSegment] intersects with an [other] iff the intersection
+   * consists of exactly one point.
+   */
+  bool intersects(LineSegment other) {
+    if (this == other) return false;
+    if (_fastExcludeIntersect(other)) return false;
+    if (this.connectsTo(other) && !this.overlaps(other)) return true;
+    var o1 = _orientation(_start, _end, other._start);
+    var o2 = _orientation(_start, _end, other._end);
+    if (o1 == o2) return false;
+    o1 = _orientation(other._start, other._end, _start);
+    o2 = _orientation(other._start, other._end, _end);
+    return o1 != o2;
+  }
+
+  /**
+   * This [LineSegment] connects to [other] iff the intersection consists
+   * of exactly one end point.
+   */
+  bool connectsTo(LineSegment other) {
+    return this != other
+        && (other.isEndPoint(_start) || other.isEndPoint(_end));
+  }
+
+  /**
+   * This [LineSegment] is colinear with [other] iff this and [other]
+   * lie on a straight line.
+   *
+   */
+  bool isColinear(LineSegment other) {
+    var o1 = _orientation(_start, _end, other._start);
+    var o2 = _orientation(_start, _end, other._end);
+    return o1 == 0 && o2 == 0;
+  }
+
+  /**
+   * This [LineSegment] and [other] overlap iff their intersection
+   * consists of more than one point.
+   */
+  bool overlaps(LineSegment other) {
+    if (this == other) return true;
+    if (!isColinear(other)) return false;
+    if (other.start < start && start < other.end) return true;
+    if (other.start < end && end < other.end) return true;
+    return false;
+  }
+
+  bool operator==(LineSegment other) =>
+      start == other.start && end == other.end;
+
+  bool get isHorizontal => _start.y == _end.y;
+
+  /**
+   * Returns the intersection point with the horizontal sweep line
+   * given by "height" [y] or null, if this segment doesn't intersect
+   * with the sweep line.
+   *
+   * Throws [StateError] if this segment is horizontal.
+   */
+  DirectPosition2D intersectionWithSweepline(num y) {
+    if (isHorizontal) {
+      throw new StateError("intersection with horizontal line not possible");
+    }
+    // note: start.y > end.y is an invariant
+    if (y > _start.y || y < _end.y) return null;
+    if (y == _start.y) return _start;
+    if (y == _end.y) return _end;
+    var slope = 1 / slope;
+    var dx = slope * (y - _start.y);
+    return new DirectPosition2D(_start.x + dx, y);
+  }
+
+  /**
+   * Returns the intersection point of this segment with the segment
+   * [other] or null, if the segments don't intersection or if they
+   * overlap
+   */
+  DirectPosition2D intersectionWith(LineSegment other) {
+    // formula and nomenclatura from
+    // from http://bloggingmath.wordpress.com/2009/05/29/line-segment-intersection/
+    var p = start;
+    var r = end - start;
+    var q = other.start;
+    var s = other.end - other.start;
+    var rs = det(r,s);
+    if (rs == 0) {
+      if (isEndPoint(other.start)) return other.start;
+      if (isEndPoint(other.end)) return other.end;
+      // no proper intersection, because this and other are
+      // colinear
+      return null;
+    }
+    // fast check whether this segments are connected
+    // (a common szenario in our data)
+    if (isEndPoint(other.start)) return other.start;
+    if (isEndPoint(other.end)) return other.end;
+
+    // properly compute the intersection point, if any.
+    var t = det(q-p, s) / rs;
+    var u = det(q-p, r) / rs;
+    if (t < 0 || t > 1 || u < 0 || u > 1) {
+      // no intersection
+      return null;
+    }
+    var intersection = p + r.scale(t);
+    return intersection;
+  }
+
+  double get slope => (_end.y - _start.y).toDouble() /
+                      (_end.x - _start.x).toDouble();
+
+  /// the start point of this segment
+  DirectPosition2D get start => _start;
+
+  ///  the end point of this segment
+  DirectPosition2D get end => _end;
+
+  /// caches the orientation value
+  num _ccwOrientation = null;
+
+  /**
+   * Returns a number in the range [-1,1]. A horizontal line
+   * has the orientation 1, a vertical line has the orientation 0.
+   *
+   * If -1 < value < 0, then the segment has a positive slope, the smaller
+   * the value, the flatter the line segment.
+   *
+   * If 0 < value < 1, then the segment has a negative slope, the
+   * higher the value, the latter the line segment.
+   */
+  num get counterclockwiseOrientation {
+    if (_ccwOrientation!= null) return _ccwOrientation;
+    if (isHorizontal) return 1;
+    var dx = end.x - start.x;
+    var dy = end.y - start.y;
+    var c = math.sqrt(dx * dx + dy * dy);
+    _ccwOrientation = dx / c;
+    return _ccwOrientation;
+  }
+
+
+  String toString() => "{LineSegment: start=$start, end=$end}";
+}
+
+/**
+ * Represent an event to be processed in the Bentley-Ottman-Algorithm
+ */
+class _Event implements Comparable<_Event> {
+  final DirectPosition2D pos;
+  List<LineSegment> _segments;
+  _Event(this.pos, [this._segments=null]);
+
+  int compareTo(_Event other) => _comparePositionsInEventOrder(pos, other.pos);
+
+  add(LineSegment segment) {
+    if (_segments == null) _segments = new List<LineSegment>();
+    if (!_segments.contains(segment)) _segments.add(segment);
+  }
+
+  List<LineSegment> get segments => _segments == null ? [] :  _segments;
+
+  bool operator ==(other) => compareTo(other) == 0;
+  int get hashCode => pos.hashCode;
+
+  toString() => "{event: pos=$pos, segments=$_segments}";
+}
+
+_comparePositionsInEventOrder(p1, p2) {
+  _require(p1 != null);
+  _require(p2 != null);
+  int c = p1.y.compareTo(p2.y);
+  // The "higher" the event, the "earlier" it is processed
+  // p1 < p2 if p1.y > p2.y
+  // p1 > p2 if p1.y < p2.y
+  if (c != 0) return -c;
+
+  // The "more left" the event, the "earlier" it is processed
+  return p1.x.compareTo(p2.x);
+}
+
+class _EventQueue {
+  /// both key and value are events. Having events as keys
+  /// ensure that they are ordered in
+  /// comparePositionsInEventOrder. Having events as values ensures
+  /// that we can maintain a list of segments starting at the
+  /// events position
+  var _events = new SplayTreeMap<_Event, _Event>();
+
+  _EventQueue();
+
+  /**
+   * Adds a new event at position [pos], unless there is already
+   * an event at this position in the queue.
+   */
+  addEvent(DirectPosition2D pos) {
+    _require(pos != null);
+    var evt = new _Event(pos);
+    _events.putIfAbsent(evt, () => evt);
+  }
+
+  /**
+   * Adds two events at the start and end position of [segment].
+   */
+  addLineSegmentEvents(LineSegment segment) {
+    _require(segment != null);
+    addEvent(segment.start);
+    addEvent(segment.end);
+    _events[new _Event(segment.start)].add(segment);
+  }
+
+  /**
+   * Returns true if this queue is empty
+   */
+  bool get isEmpty => _events.isEmpty;
+
+  /**
+   * Removes the first event from the queue and returns it.
+   * Throws an [StateError] if the queue is empty.
+   */
+  _Event unshift() {
+    if (isEmpty) throw new StateError();
+    var key = _events.firstKey();
+    var ret = _events[key];
+    _events.remove(key);
+    return ret;
+  }
+
+  /**
+   * Returns the first event in this queue without removing it
+   * from this queue.
+   *
+   * Throws a [StateError] if the queue is empty.
+   */
+  _Event get first {
+    if (this.isEmpty) throw new StateError("queue is empty");
+    var evt = _events.firstKey();
+    return _events[evt];
+  }
+
+  int get length => _events.length;
+}
+
+class _SweepLineCompareFunction {
+  DirectPosition2D event;
+  int sign = 1;
+  /// the compare function used for compare operations just *before*
+  /// the sweep line goes through the event position
+  _SweepLineCompareFunction.atMinusEpsilon(this.event): sign = -1;
+
+  /// the compare function used for compare operations just *after*
+  /// the sweep line went through the event position
+  _SweepLineCompareFunction.atPlusEpsilon(this.event): sign = 1;
+  int call(LineSegment value, LineSegment other) {
+    // if other is left or right of the reference point
+    // 'event' (which a priori is known to be on the segment
+    // 'value' too), the ordering is clear
+    var o = _orientation(other.start, other.end, event);
+    if (o != 0) return o;
+
+    // otherwise 'value' and 'other' intersect in the
+    // point event and are ordered according to the counter clockwise orientation
+    // value. If both values are identical, then 'value'
+    // and 'other' overlap (i.e. all their endpoints are colineaer and the
+    // intersection isn't empty). They are considered to be in an
+    // equivalence class.
+    return sign * value.counterclockwiseOrientation.compareTo(other.counterclockwiseOrientation);
+  }
+}
+
+/**
+ * A [LineIntersection] represents te interesection of two or more
+ * [LineSegment]s.
+ */
+class LineIntersection {
+  /// the position where the line segments intersect
+  final DirectPosition2D pos;
+  List<LineSegment> _intersecting;
+
+  /// the list of intersecting line segments
+  List<LineSegment> get intersecting => new UnmodifiableListView(_intersecting);
+
+  LineIntersection._(this.pos, Iterable<LineSegment> intersecting) {
+    _require(intersecting != null);
+    this._intersecting = new List.from(intersecting, growable: false);
+  }
+
+  toString() => "{intersection: pos=$pos, segments=${_intersecting.join(",")}";
+}
+
+final _logger = LoggerFactory.getLogger("LineIntersection");
+
+/**
+ * Computes the line intersections for the collection of line segments in
+ * [segments].
+ *
+ * Returns an empty [Iterable], if no interesections have been detected in
+ * [segments].
+ */
+Iterable<LineIntersection> computeLineIntersections(Iterable<LineSegment> segments) {
+
+  /*
+   * Implementation and terminology closely follows
+   *    M. van de Berg
+   *    Computational Geometry
+   *    Springer
+   *    1998
+   *    Chapter 2, Line Segment Intersection
+   */
+
+
+  final AvlTree<LineSegment> sweepLine = new AvlTree<LineSegment>();
+  final eventQueue = new _EventQueue();
+  final intersections = new List<LineIntersection>();
+
+
+  /// initializes the event queue with the start events of the segments
+  initEventQueue() {
+    segments.forEach((s) {
+      eventQueue.addLineSegmentEvents(s);
+    });
+  }
+
+  Iterable flatten(e) => e is Iterable
+      ? e.expand(flatten)
+      : new List.filled(1, e);
+
+  findAndRememberEvent(sl, sr, pos) {
+    if (sl is Iterable) sl = sl.isEmpty ? null : sl.first;
+    if (sr is Iterable) sr = sr.isEmpty ? null : sr.first;
+    if (sl == null) return;
+    if (sr == null) return;
+    var intersection = sl.intersectionWith(sr);
+    if (intersection == null) return;
+    if (_comparePositionsInEventOrder(intersection, pos) > 0) {
+      eventQueue.addEvent(intersection);
+    }
+  }
+
+  handleEvent(_Event event) {
+    // the list of events starting at this point
+    var U = new List.from(event.segments, growable:false);
+
+    // the list of segments interesecting with the sweepline at point
+    // event.pos
+    var t = sweepLine
+        .inorderEqualOrLarger((s) => _orientation(s.start, s.end, event.pos))
+        .takeWhile((s) => _orientation(s.start, s.end, event.pos) == 0)
+        .toList(growable:false);
+
+    // the segments ending in event.pos
+    var L = t.where((s) => s.end == event.pos);
+
+    // the segments properly containing event.pos (event.pos is neither
+    // the start nor the end point)
+    var C = t.where((s) => s.end != event.pos);
+
+    if (U.length + L.length + C.length > 1) {
+      // create a new line intersection at position event.pos
+      // and the intersecting lines
+      var intersecting = flatten([U, L, C]).toSet();
+      intersections.add(new LineIntersection._(event.pos, intersecting));
+    }
+
+    var compare = new _SweepLineCompareFunction.atMinusEpsilon(event.pos);
+
+    L.forEach((s) => sweepLine.remove(s, compare: compare));
+    C.forEach((s) =>sweepLine.remove(s, compare: compare));
+
+    compare = new _SweepLineCompareFunction.atPlusEpsilon(event.pos);
+    U.forEach((s) => sweepLine.add(s, compare:compare));
+    C.forEach((s) => sweepLine.add(s, compare:compare));
+
+    compare = (LineSegment other) => _orientation(other.start, other.end, event.pos);
+
+    if (U.length + C.length == 0) {
+      var sl = sweepLine.leftNeighbour(compare);
+      var sr = sweepLine.rightNeighbour(compare);
+        findAndRememberEvent(sl, sr, event.pos);
+    } else {
+
+    }
+    if (U.length + C.length == 0) {
+      var sl = sweepLine.leftNeighbour(compare);
+      var sr = sweepLine.rightNeighbour(compare);
+      findAndRememberEvent(sl, sr, event.pos);
+    } else {
+      var UC = flatten([U, C]).toList(growable: false);
+      var sl = sweepLine.leftNeighbour(compare);
+      var sr = sweepLine.rightNeighbour(compare);
+      compare = new _SweepLineCompareFunction.atPlusEpsilon(event.pos);
+      UC.sort(compare);
+      findAndRememberEvent(sl, UC.first, event.pos);
+      findAndRememberEvent(sr, UC.last, event.pos);
+    }
+  }
+
+  initEventQueue();
+  while(!eventQueue.isEmpty) {
+    var event = eventQueue.unshift();
+    handleEvent(event);
+  }
+  return intersections;
+}
+
+
--- test/test_line_intersection.dart
+++ test/test_line_intersection.dart
@@ -1,7 +1,12 @@
 library test_line_intersection;
 
+import "package:avl_tree/avl_tree.dart";
 import "package:unittest/unittest.dart";
-import "../lib/line_intersection.dart";
+import "dart:collection";
+import "dart:math" as math;
+
+part "../lib/src/direct_position.dart";
+part "../lib/src/line_intersection.dart";
 
 main() {
   group("orientation -", () {
@@ -9,14 +14,14 @@ main() {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(5,2);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, 1);
     });
     test("clock-wise", () {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(5,-2);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, -1);
     });
 
@@ -24,7 +29,7 @@ main() {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(20,0);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, 0);
     });
 
@@ -32,7 +37,7 @@ main() {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(5,0);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, 0);
     });
 
@@ -40,7 +45,7 @@ main() {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(10,0);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, 0);
     });
 
@@ -48,7 +53,7 @@ main() {
       var p = new DirectPosition2D(0,0);
       var q = new DirectPosition2D(10,0);
       var r = new DirectPosition2D(20,1E10-10);
-      var o = orientation(p,q,r);
+      var o = _orientation(p,q,r);
       expect(o, 1);
     });
   });
@@ -119,6 +124,43 @@ main() {
     });
   });
 
+  group("segment intersection -", () {
+    test("two intersecting segments", () {
+      var s1 = new LineSegment.from([-2,0], [2,0]);
+      var s2 = new LineSegment.from([0,2], [0,-2]);
+      var intersection = s1.intersectionWith(s2);
+      expect(intersection, equals(new DirectPosition2D(0,0)));
+    });
+
+    test("two non-intersecting segments", () {
+      var s1 = new LineSegment.from([-2,0], [2,0]);
+      var s2 = new LineSegment.from([1,1], [5,5]);
+      var intersection = s1.intersectionWith(s2);
+      expect(intersection, isNull);
+    });
+
+    test("two colinear, overlapping segments", () {
+      var s1 = new LineSegment.from([-2,0], [2,0]);
+      var s2 = new LineSegment.from([0,0], [4,0]);
+      var intersection = s1.intersectionWith(s2);
+      expect(intersection, isNull);
+    });
+
+    test("two connected segments, colinaear, connected at end", () {
+      var s1 = new LineSegment.from([-2,0], [2,0]);
+      var s2 = new LineSegment.from([2,0], [4,0]);
+      var intersection = s1.intersectionWith(s2);
+      expect(intersection, new DirectPosition2D(2,0));
+    });
+
+    test("two connected segments, non-colienar, connected at start", () {
+      var s1 = new LineSegment.from([-2,-2], [0,0]);
+      var s2 = new LineSegment.from([0,0], [4,7]);
+      var intersection = s1.intersectionWith(s2);
+      expect(intersection, new DirectPosition2D(0,0));
+    });
+  });
+
   group("interesection with sweep line -", () {
     test("of a vertical line", () {
        var s1 = new LineSegment.from([0,0], [0,10]);
@@ -143,17 +185,17 @@ main() {
   });
 
 
-  group("EventQueue -", () {
+  group("_EventQueue -", () {
     group("constructor -", () {
        test("create an event queue", () {
-         var queue = new EventQueue();
+         var queue = new _EventQueue();
          expect(queue.isEmpty, true);
        });
     });
 
     group("addEvent -", () {
       test("add a non-exitent event", () {
-        var queue = new EventQueue();
+        var queue = new _EventQueue();
         queue.addEvent(new DirectPosition2D(0,1));
         expect(queue.length, 1);
         expect(queue.first.pos.x, 0);
@@ -162,7 +204,7 @@ main() {
       });
 
       test("add an already existing event", () {
-        var queue = new EventQueue();
+        var queue = new _EventQueue();
         queue.addEvent(new DirectPosition2D(0,1));
         queue.addEvent(new DirectPosition2D(0,1));
         expect(queue.length, 1);
@@ -174,7 +216,7 @@ main() {
 
     group("addLineSegmentEvents -", () {
       test("for a segment", () {
-        var queue = new EventQueue();
+        var queue = new _EventQueue();
         var ls = new LineSegment(
             new DirectPosition2D(0,0),
             new DirectPosition2D(10,10)
@@ -191,7 +233,7 @@ main() {
 
     group("unshift -", () {
       test("from a queue with one event", () {
-        var queue = new EventQueue();
+        var queue = new _EventQueue();
         queue.addEvent(new DirectPosition2D(0,1));
 
         var event = queue.unshift();
@@ -201,7 +243,7 @@ main() {
       });
 
       test("from a queue with one event", () {
-        var queue = new EventQueue();
+        var queue = new _EventQueue();
         var ls = new LineSegment(
             new DirectPosition2D(0,0),
             new DirectPosition2D(10,10)
@@ -215,8 +257,38 @@ main() {
         expect(queue.first.segments.isEmpty, true);
       });
     });
-
   });
 
-}
+  group("flatten -", () {
+    Iterable flatten(e) => e is Iterable
+        ? e.expand(flatten)
+            : new List.filled(1, e);
 
+      test("flatten", () {
+        expect(flatten([1,2]).toList(), equals([1,2]));
+        expect(flatten([1,[2,3],4]).toList(), equals([1,2,3,4]));
+        expect(flatten([1,[2,[3,4,5], [6], []],7]).toList(), equals([1,2,3,4,5,6,7]));
+      });
+    });
+
+  group("line intersection -", () {
+    test("simple case - two lines intersecting in one point", () {
+      var s1 = new LineSegment.from([-5,5], [5,-5]);
+      var s2 = new LineSegment.from([2,2], [-2,-2]);
+      var intersections = computeLineIntersections([s1,s2]);
+      expect(intersections.length, 1);
+      expect(intersections.first.pos, equals(new DirectPosition2D(0, 0)));
+      expect(intersections.first.intersecting.toSet(), equals([s1, s2].toSet()));
+    });
+
+
+    test("simple case - two lines being connected in one point", () {
+      var s1 = new LineSegment.from([0,0], [5,0]);
+      var s2 = new LineSegment.from([5,0], [7,7]);
+      var intersections = computeLineIntersections([s1,s2]);
+      expect(intersections.length, 1);
+      expect(intersections.first.pos, equals(new DirectPosition2D(5, 0)));
+      expect(intersections.first.intersecting.toSet(), equals([s1, s2].toSet()));
+    });
+  });
+}
